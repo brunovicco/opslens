8ad434f381935cc5cac1c9522b62b894a5b6fc7c
Bruno Vicco <bfvicco@gmail.com>
2026-08-13 18:45:41 -0300

    infra: abort incomplete state uploads

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -45,6 +45,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "terraform_state" {
     noncurrent_version_expiration {
       noncurrent_days = 90
     }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
 
