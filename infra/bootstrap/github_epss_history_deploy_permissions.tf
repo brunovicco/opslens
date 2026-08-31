aba6e969cc49b706d3be2988d78c7ac855f48858
Bruno Vicco <bfvicco@gmail.com>
2026-08-31 11:54:39 -0300

    fix(iam): allow scoped history log tagging

--- infra/bootstrap/github_epss_history_deploy_permissions.tf
+++ infra/bootstrap/github_epss_history_deploy_permissions.tf
@@ -168,7 +168,10 @@ data "aws_iam_policy_document" "github_actions_epss_history_deploy" {
   statement {
     sid     = "TagEpssHistoryTransformerLogGroupOnCreate"
     effect  = "Allow"
-    actions = ["logs:TagResource"]
+    actions = [
+      "logs:TagLogGroup",
+      "logs:TagResource",
+    ]
 
     resources = [
       local.dev_epss_history_transformer_log_group_arn,
