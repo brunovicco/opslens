908937fdf4b37c9348e9a4509b58b0668c3ba0e7
Bruno Vicco <bfvicco@gmail.com>
2026-09-02 15:58:23 -0300

    feat(repository): export PyPI normalization bridge

--- src/opslens/repository_intelligence/application/__init__.py
+++ src/opslens/repository_intelligence/application/__init__.py
@@ -4,6 +4,9 @@ from opslens.repository_intelligence.application.file_acquisition import (
     GitHubUvLockSource,
     acquire_uv_lock_evidence,
 )
+from opslens.repository_intelligence.application.pypi_normalization import (
+    normalize_uv_lock_pypi_dependencies,
+)
 from opslens.repository_intelligence.application.snapshot_resolution import (
     GitHubRepositorySnapshotSource,
     GitHubSnapshotResolutionEvidence,
@@ -15,5 +18,6 @@ __all__ = [
     "GitHubSnapshotResolutionEvidence",
     "GitHubUvLockSource",
     "acquire_uv_lock_evidence",
+    "normalize_uv_lock_pypi_dependencies",
     "resolve_github_repository_snapshot",
 ]
