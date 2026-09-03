dbb5290f3c4f0a1da7b2a1570d0f047559696008
Bruno Vicco <bfvicco@gmail.com>
2026-09-03 14:26:34 -0300

    feat(repository): export NVD enrichment application service

--- src/opslens/repository_intelligence/application/__init__.py
+++ src/opslens/repository_intelligence/application/__init__.py
@@ -4,6 +4,9 @@ from opslens.repository_intelligence.application.file_acquisition import (
     GitHubUvLockSource,
     acquire_uv_lock_evidence,
 )
+from opslens.repository_intelligence.application.nvd_enrichment import (
+    enrich_repository_findings_with_nvd,
+)
 from opslens.repository_intelligence.application.pypi_normalization import (
     normalize_uv_lock_pypi_dependencies,
 )
@@ -22,6 +25,7 @@ __all__ = [
     "GitHubUvLockSource",
     "acquire_uv_lock_evidence",
     "build_repository_pypi_vulnerability_scan",
+    "enrich_repository_findings_with_nvd",
     "normalize_uv_lock_pypi_dependencies",
     "resolve_github_repository_snapshot",
 ]
