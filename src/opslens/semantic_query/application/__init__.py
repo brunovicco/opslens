c67c2564bfb18b4c00c90ab654582e4241f0c121
Bruno Vicco <bfvicco@gmail.com>
2026-09-04 13:59:24 -0300

    feat(semantic-query): export natural-language execution use case

--- src/opslens/semantic_query/application/__init__.py
+++ src/opslens/semantic_query/application/__init__.py
@@ -3,9 +3,23 @@
 from opslens.semantic_query.application.compiler import compile_semantic_query
 from opslens.semantic_query.application.executor import ExecuteSemanticQuery
 from opslens.semantic_query.application.models import AthenaQueryResult
+from opslens.semantic_query.application.natural_language import (
+    ExecutedNaturalLanguageSemanticQuery,
+    ExecuteNaturalLanguageSemanticQuery,
+    NaturalLanguageSemanticQueryResult,
+    SemanticQueryPlanner,
+    TypedSemanticQueryExecutor,
+    UnsupportedNaturalLanguageSemanticQuery,
+)
 
 __all__ = [
     "AthenaQueryResult",
+    "ExecutedNaturalLanguageSemanticQuery",
+    "ExecuteNaturalLanguageSemanticQuery",
     "ExecuteSemanticQuery",
+    "NaturalLanguageSemanticQueryResult",
+    "SemanticQueryPlanner",
+    "TypedSemanticQueryExecutor",
+    "UnsupportedNaturalLanguageSemanticQuery",
     "compile_semantic_query",
 ]
