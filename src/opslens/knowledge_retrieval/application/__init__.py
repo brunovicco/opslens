66bc744e508b5212d0fa35775021697eae5fceeb
Bruno Vicco <bfvicco@gmail.com>
2026-09-04 21:36:43 -0300

    feat(knowledge-retrieval): export corpus config loaders

--- src/opslens/knowledge_retrieval/application/__init__.py
+++ src/opslens/knowledge_retrieval/application/__init__.py
@@ -1,5 +1,10 @@
 """Application services for deterministic knowledge-corpus materialization."""
 
+from opslens.knowledge_retrieval.application.corpus_config import (
+    CorpusConfigError,
+    load_corpus_spec,
+    load_source_registry,
+)
 from opslens.knowledge_retrieval.application.corpus_materialization import (
     CanonicalSourceTextError,
     materialize_knowledge_document,
@@ -9,6 +14,9 @@ from opslens.knowledge_retrieval.application.corpus_materialization import (
 
 __all__ = [
     "CanonicalSourceTextError",
+    "CorpusConfigError",
+    "load_corpus_spec",
+    "load_source_registry",
     "materialize_knowledge_document",
     "normalize_source_text",
     "select_exact_section",
