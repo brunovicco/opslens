40f0761d62175b9bb0a753cd58f1f39b907c596c
Bruno Vicco <bfvicco@gmail.com>
2026-09-07 01:11:00 -0300

    feat(phase9): export public semantic planning domain

--- src/opslens/public_analysis/domain/__init__.py
+++ src/opslens/public_analysis/domain/__init__.py
@@ -11,13 +11,29 @@ from opslens.public_analysis.domain.request import (
     PublicRepositoryTarget,
     create_public_analysis_request,
 )
+from opslens.public_analysis.domain.semantic_planning import (
+    PUBLIC_ANALYSIS_HANDOFF_CONTRACT_VERSION,
+    PUBLIC_ANALYSIS_OPERATION,
+    PUBLIC_ANALYSIS_V1_REQUIRED_EVIDENCE_NEEDS,
+    PUBLIC_SEMANTIC_PLANNING_CONTRACT_VERSION,
+    PublicAnalysisAdmissionHandoff,
+    PublicSemanticPlanningRequest,
+    PublicSemanticPlanProposal,
+)
 
 __all__ = [
+    "PUBLIC_ANALYSIS_HANDOFF_CONTRACT_VERSION",
+    "PUBLIC_ANALYSIS_OPERATION",
     "PUBLIC_ANALYSIS_REQUEST_CONTRACT_VERSION",
+    "PUBLIC_ANALYSIS_V1_REQUIRED_EVIDENCE_NEEDS",
     "PUBLIC_REPOSITORY_EVIDENCE_CONTRACT_VERSION",
+    "PUBLIC_SEMANTIC_PLANNING_CONTRACT_VERSION",
+    "PublicAnalysisAdmissionHandoff",
     "PublicAnalysisRequest",
     "PublicAnalysisValidationError",
     "PublicRepositoryEvidenceExecution",
     "PublicRepositoryTarget",
+    "PublicSemanticPlanningRequest",
+    "PublicSemanticPlanProposal",
     "create_public_analysis_request",
 ]
