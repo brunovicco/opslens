c991de2e88f02b2bcbea9af7ab16e3af93b24b38
Bruno Vicco <bfvicco@gmail.com>
2026-09-07 12:39:32 -0300

    feat(phase10): export operational orchestration boundary

--- src/opslens/public_analysis/application/__init__.py
+++ src/opslens/public_analysis/application/__init__.py
@@ -4,6 +4,15 @@ from opslens.public_analysis.application.evidence_orchestration import (
     PublicRepositoryEvidenceSource,
     build_public_repository_evidence,
 )
+from opslens.public_analysis.application.operational_orchestration import (
+    MonotonicClock,
+    OperationalEventSink,
+    PublicAnalysisInstrumentationError,
+    PublicAnalysisInstrumentationFailure,
+    PublicAnalysisOperationalExecution,
+    PublicAnalysisOperationalFailure,
+    execute_instrumented_public_analysis,
+)
 from opslens.public_analysis.application.request_admission import (
     MAX_PUBLIC_ANALYSIS_REQUEST_BYTES,
     MAX_PUBLIC_REPOSITORY_URL_CHARS,
@@ -16,15 +25,23 @@ from opslens.public_analysis.application.semantic_planning import (
     PublicSemanticPlanAdmissionError,
     PublicSemanticPlanner,
     admit_public_semantic_plan,
+    build_public_analysis_admission_handoff,
     build_public_semantic_planning_request,
     parse_public_semantic_plan_proposal,
     plan_public_analysis_handoff,
+    route_public_semantic_plan,
 )
 
 __all__ = [
     "MAX_PUBLIC_ANALYSIS_REQUEST_BYTES",
     "MAX_PUBLIC_REPOSITORY_URL_CHARS",
     "MAX_PUBLIC_SEMANTIC_PLAN_RESPONSE_BYTES",
+    "MonotonicClock",
+    "OperationalEventSink",
+    "PublicAnalysisInstrumentationError",
+    "PublicAnalysisInstrumentationFailure",
+    "PublicAnalysisOperationalExecution",
+    "PublicAnalysisOperationalFailure",
     "PublicAnalysisRequestAdmission",
     "PublicAnalysisRequestAdmissionError",
     "PublicRepositoryEvidenceSource",
@@ -32,8 +49,11 @@ __all__ = [
     "PublicSemanticPlanner",
     "admit_public_analysis_request",
     "admit_public_semantic_plan",
+    "build_public_analysis_admission_handoff",
     "build_public_repository_evidence",
     "build_public_semantic_planning_request",
+    "execute_instrumented_public_analysis",
     "parse_public_semantic_plan_proposal",
     "plan_public_analysis_handoff",
+    "route_public_semantic_plan",
 ]
