761aec889787a23e157de94805db6c63760e8d1a
Bruno Vicco <bfvicco@gmail.com>
2026-09-09 23:12:30 -0300

    hardening(phase17): disable NVD promotion trace payload capture

--- src/opslens/transformation/nvd/promotion_lambda_handler.py
+++ src/opslens/transformation/nvd/promotion_lambda_handler.py
@@ -97,7 +97,7 @@ type NvdPromotionLambdaResponse = (
     clear_state=True,
     log_event=False,
 )
-@tracer.capture_lambda_handler
+@tracer.capture_lambda_handler(capture_response=False, capture_error=False)
 @metrics.log_metrics
 def lambda_handler(
     event: Mapping[str, object],
