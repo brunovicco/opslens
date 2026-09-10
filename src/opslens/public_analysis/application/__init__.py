6eabdb7e45f60595bef1d8042fcbff1dbcf2c8c6
Bruno Vicco <bfvicco@gmail.com>
2026-09-10 17:51:00 -0300

    feat(phase19): export representative measurement harness

--- src/opslens/public_analysis/application/__init__.py
+++ src/opslens/public_analysis/application/__init__.py
@@ -13,6 +13,13 @@ from opslens.public_analysis.application.operational_orchestration import (
     PublicAnalysisOperationalFailure,
     execute_instrumented_public_analysis,
 )
+from opslens.public_analysis.application.representative_measurement import (
+    AdmittedResultSerializer,
+    MeasurementClock,
+    RepresentativeStageAction,
+    RepresentativeWorkloadPlan,
+    measure_representative_workload,
+)
 from opslens.public_analysis.application.request_admission import (
     MAX_PUBLIC_ANALYSIS_REQUEST_BYTES,
     MAX_PUBLIC_REPOSITORY_URL_CHARS,
@@ -36,6 +43,8 @@ __all__ = [
     "MAX_PUBLIC_ANALYSIS_REQUEST_BYTES",
     "MAX_PUBLIC_REPOSITORY_URL_CHARS",
     "MAX_PUBLIC_SEMANTIC_PLAN_RESPONSE_BYTES",
+    "AdmittedResultSerializer",
+    "MeasurementClock",
     "MonotonicClock",
     "OperationalEventSink",
     "PublicAnalysisInstrumentationError",
@@ -47,12 +56,15 @@ __all__ = [
     "PublicRepositoryEvidenceSource",
     "PublicSemanticPlanAdmissionError",
     "PublicSemanticPlanner",
+    "RepresentativeStageAction",
+    "RepresentativeWorkloadPlan",
     "admit_public_analysis_request",
     "admit_public_semantic_plan",
     "build_public_analysis_admission_handoff",
     "build_public_repository_evidence",
     "build_public_semantic_planning_request",
     "execute_instrumented_public_analysis",
+    "measure_representative_workload",
     "parse_public_semantic_plan_proposal",
     "plan_public_analysis_handoff",
     "route_public_semantic_plan",
