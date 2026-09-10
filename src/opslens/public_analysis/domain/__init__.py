f2e14e698047415f1da4ef4fbd69137d333f6d49
Bruno Vicco <bfvicco@gmail.com>
2026-09-10 17:50:48 -0300

    feat(phase19): export representative measurement contracts

--- src/opslens/public_analysis/domain/__init__.py
+++ src/opslens/public_analysis/domain/__init__.py
@@ -5,6 +5,15 @@ from opslens.public_analysis.domain.evidence_execution import (
     PUBLIC_REPOSITORY_EVIDENCE_CONTRACT_VERSION,
     PublicRepositoryEvidenceExecution,
 )
+from opslens.public_analysis.domain.representative_measurement import (
+    REPRESENTATIVE_PUBLIC_ANALYSIS_WORKLOAD_ID,
+    REPRESENTATIVE_WORKLOAD_STAGE_ORDER,
+    ProviderResourceUsage,
+    RepresentativeStageMeasurement,
+    RepresentativeWorkloadMeasurement,
+    RepresentativeWorkloadStage,
+    sum_provider_usage,
+)
 from opslens.public_analysis.domain.request import (
     PUBLIC_ANALYSIS_REQUEST_CONTRACT_VERSION,
     PublicAnalysisRequest,
@@ -30,6 +39,9 @@ __all__ = [
     "PUBLIC_ANALYSIS_V1_REQUIRED_EVIDENCE_NEEDS",
     "PUBLIC_REPOSITORY_EVIDENCE_CONTRACT_VERSION",
     "PUBLIC_SEMANTIC_PLANNING_CONTRACT_VERSION",
+    "REPRESENTATIVE_PUBLIC_ANALYSIS_WORKLOAD_ID",
+    "REPRESENTATIVE_WORKLOAD_STAGE_ORDER",
+    "ProviderResourceUsage",
     "PublicAnalysisAdmissionHandoff",
     "PublicAnalysisRequest",
     "PublicAnalysisValidationError",
@@ -37,5 +49,9 @@ __all__ = [
     "PublicRepositoryTarget",
     "PublicSemanticPlanProposal",
     "PublicSemanticPlanningRequest",
+    "RepresentativeStageMeasurement",
+    "RepresentativeWorkloadMeasurement",
+    "RepresentativeWorkloadStage",
     "create_public_analysis_request",
+    "sum_provider_usage",
 ]
