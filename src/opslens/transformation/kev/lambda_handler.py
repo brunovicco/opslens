0edef6e2954b561bd5dbbd015a33641fd0e087b8
Bruno Vicco <bfvicco@gmail.com>
2026-09-09 23:09:34 -0300

    hardening(phase17): disable KEV Silver trace payload capture

--- src/opslens/transformation/kev/lambda_handler.py
+++ src/opslens/transformation/kev/lambda_handler.py
@@ -81,7 +81,7 @@ class KevSilverInvocationResponse(TypedDict):
     clear_state=True,
     log_event=False,
 )
-@tracer.capture_lambda_handler
+@tracer.capture_lambda_handler(capture_response=False, capture_error=False)
 @metrics.log_metrics
 def lambda_handler(
     event: Mapping[str, object],
